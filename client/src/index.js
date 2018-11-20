7a3a978338dceaaccaead0d6d03507137d7a60d6
kendallherron <ktherron@valdosta.edu>
2018-11-19 22:27:06 -0500

    Navbar

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,19 @@
-import React from 'react';
+//import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import NavBar from './components/Navbar';
+import React, { Component } from "react";
+
+class App extends Component {
+	constructor(props){
+
+	
+	    return (
+	        <div>
+	            <NavBar />
+	        </div>
+	    );
+	}
+};
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
