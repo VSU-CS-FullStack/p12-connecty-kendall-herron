0abf7b28b5097e926513467f66df7a9506d7184e
kendallherron <ktherron@valdosta.edu>
2018-11-19 23:18:21 -0500

    React Router Setup / , /register

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,29 @@
 import React, { component } from 'react';
 import './App.css';
-import NavBar from "./components/Navbar";
-import Footer from "./components/Footer";
-import Landing from "./components/Landing";
+import NavBar from "./components/navbar";
+import Footer from "./components/footer";
+import Landing from "./components/landing";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Register from "./components/register";
 
 
 
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-      	<Navbar />
-        <Landing />
-        <Footer />
-      </div>
-    );
-  }
+ render() {
+        return (
+            <BrowserRouter>
+                <div className="App">
+                    <Navbar />
+                    <Switch>
+                    	<Route path="/register" component={Register} />
+                        <Route exact path="/" component={Landing} />
+                    </Switch>
+                    <Footer />
+                </div>
+            </BrowserRouter>
+        );
+    }
 }
 
 export default App;
