47253abdf3509d27d14180a5e8ca39b47eb5026a
kendallherron <ktherron@valdosta.edu>
2018-12-02 22:17:07 -0500

    React component

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 
+import { Provider } from "react-redux";
 import store from "./store";
 
 
--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,14 +1,127 @@
 import React, { Component } from 'react';
+// import axios from 'axios';
+import classnames from 'classnames';
+import { connect } from "react-redux";
+import { loginUser } from "../../actions/authActions";
+import PropTypes from "prop-types";
 
 class Login extends Component {
+    constructor(props){
+		super(props);
+
+		this.state = {
+        email: "",
+        password: "",
+        error: {} // will be explained later
+    	};
+
+    	this.onInputChange = this.onInputChange.bind(this);
+		this.onFormSubmit = this.onFormSubmit.bind(this);
+
+	}
+
+	componentWillReceiveProps(nextProps) {
+            if (nextProps.errors) {
+                this.setState({ errors: nextProps.errors });
+            }
+    }
+
+	onInputChange(e) {        
+		// console.log(event.target.value);
+		this.setState({ [e.target.name]: e.target.value });
+	}
+
+	onFormSubmit(event){
+		event.preventDefault();
+
+    const newUser = {
+        email: this.state.email,
+        password: this.state.password
+    };
+
+    // axios
+    //     .post("api/users/register", newUser)
+    //     .then(res => this.props.history.push("/login"))
+    //     .catch(err => this.setState({error: err.response.data}));
+
+    this.props.loginUser(newUser, this.props.history);
+	}
+
     render() {
+    	const { error } = this.state;
     	return 	(
     		<div className="container">
             	<h1>Login</h1>
-        	</div>  
+            	<div className="login">
+				    <div className="container">
+				      <div className="row">
+				        <div className="col-md-8 m-auto">
+				          <h1 className="display-4 text-center">Login In</h1>
+				          <p className="lead text-center">Login into your Connecty account</p>
+				          <form noValidate
+    							onSubmit={this.onFormSubmit} action="create-profile.html">
+				            <div className="form-group">
+				              	<input 
+    							/>
+				            </div>
+				            <div className="form-group">
+				              	<input 
+				              		type="email" 
+				              		className={classnames("form-control form-control-lg", {'is-invalid': error.email})} 
+				              		placeholder="Email Address" 
+				              		name="email" 
+				              		value={this.state.email}
+        							onChange={this.onInputChange}
+				              	/>
+				              	{	
+                                    error.email && (
+                                        <div className="invalid-feedback">{error.email}</div>
+                                    )
+                                }
+				              <small className="form-text text-muted">This site uses Gravatar so if you want a profile image, use a Gravatar email</small>
+				            </div>
+				            <div className="form-group">
+				              	<input 
+				              		type="password" 
+				              		className={classnames("form-control form-control-lg", {'is-invalid': error.password})} 
+				              		placeholder="Password" 
+				              		name="password"
+				              		value={this.state.password}
+        							onChange={this.onInputChange}
+				              	/>
+				              	{
+                                    error.password && (
+                                        <div className="invalid-feedback">{error.password}</div>
+                                    )
+                                }
+				            </div>
+				            <div className="form-group">
+				              	<input 
+				              	/>
+				            </div>
+				            <input type="submit" className="btn btn-info btn-block mt-4" />
+				          </form>
+				        </div>
+				      </div>
+				    </div>
+				  </div>
+        	</div>		  
     			
  		);
     }
 }
 
-export default Login;
+Login.propTypes = {
+    loginUser: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired,
+    errors: PropTypes.object.isRequired
+};
+
+
+
+const mapStateToProps = state => ({
+    auth: state.auth,
+    errors: state.errors
+});
+
+export default connect(mapStateToProps, { loginUser })(Login);
